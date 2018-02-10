11c72c32e3e369e36aed67b77e951385249fb9b7
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2018-02-10 11:30:17 -0600

    Scroll animado (terminado)
    Fin de curso

--- Proyecto Final/dev/js/animated-scroll.js
+++ Proyecto Final/dev/js/animated-scroll.js
@@ -1,35 +1,128 @@
+// CODIGO DESCARGADO
+
+// const getInitialScroll = () => document.documentElement.scrollTop;
+// const getFinalScroll = element => Math.floor(element.getBoundingClientRect().top + getInitialScroll());
+
+// const animatedScrollTo = (targetElement,time) => {
+//   let initialPosition = getInitialScroll(),
+//       finalPosition = getFinalScroll(targetElement),
+//       distanceToScroll = finalPosition - initialPosition,
+//       scrollFragment = Math.ceil(distanceToScroll / time);
+//   animateScroll(scrollFragment, finalPosition);
+// };
+
+// const animateScroll = (scrollFragment,finalPosition) => {
+//   let animatedScroll = setInterval(function(){
+//     document.documentElement.scrollTop += scrollFragment;
+//     if (scrollFragment > 0) {
+//       if (document.documentElement.scrollTop > finalPosition - (scrollFragment / 2)) clearInterval(animatedScroll)
+//     } else {
+//       if (document.documentElement.scrollTop < finalPosition - (scrollFragment / 2)) clearInterval(animatedScroll)
+//     }
+
+//   },1);
+// };
+
+// const animatedScrollEvent = (originElement,time) => {
+//   if (originElement.tagName === 'A' && originElement.hash !== '') {
+//     let targetElement = document.getElementById(originElement.hash.slice(1));
+//     originElement.addEventListener('click', e => {
+//       e.preventDefault();
+//       animatedScrollTo(targetElement,time)
+//     })
+//   }
+// };
+
+// const animatedScrollAllLinks = time => {
+//   let links = document.links;
+//   for (let link of links) {
+//     animatedScrollEvent(link,time)
+//   }
+// };
+
+// animatedScrollAllLinks(200);
+
+// animatedScrollEvent(document.getElementById('link2'),500);
+
+
+// TERMINA CODIGO DESCARGADO
+
+
+/**********************************************/
+
+// console.log(document.documentElement.scrollTop);
+// console.log(window.scrollY);
+
 // let y = 0;
+// let animatedScroll = setInterval(function(){
+	// document.documentElement.scrollTop += 10;
 
-// document.body.scrollTop;
+	// Con window seria diferente es mas sencillo con document.documentElementt
+	// y += 10;
+	// window.scrollBy(0, y);
 
-const getInitialScroll = () => window.scrollY;
-// console.log(getScroll());
+// }, 10);
+
+
+const getInitialScroll = () => document.documentElement.scrollTop;
+// getBoundingClientRect detecta las coordenadas respecto al viewport
 const getFinalScroll = element => Math.floor(element.getBoundingClientRect().top + getInitialScroll());
 
 // console.log(getFinalScroll(document.getElementById('cap2')));
 
-const animatedScrollTo = (element, time) => {
+// Se necesita una unidad de tiempo para hacer la animacion
+
+const animatedScrollTo = (targetElement, time) => {
 	let initialPosition = getInitialScroll(),
-		finalPosition = getFinalScroll(element),
+		finalPosition = getFinalScroll(targetElement),
 		distanceToScroll = finalPosition - initialPosition,
+
+		// Si un auto recorre 10km (una distancia) en 1min (un tiempo) puedo obtener cuanto recorre por segundo
+		// supongamos que obtenemos 10m por segundo con Distancia / tiempo
 		scrollFragment = distanceToScroll / time;
 
-	animatedScroll(scrollFragment);
+	animateScroll(scrollFragment, finalPosition);
+	// console.log(scrollFragment);
 };
 
-const animateScroll = scrollFragment => {
-	let animatedScroll = setInterval(function(){
-		window.scrollY += scrollFragment;
+const animateScroll = (scrollFragment, finalPosition) => {
+	// Recorrera el fragmento cada 1ms
+	let animatedScroll = setInterval(function () {
+		document.documentElement.scrollTop += scrollFragment;
+
+		// Nos va dando la posicion del elemento
+		// console.log(document.documentElement.scrollTop);
+
+		if (scrollFragment > 0) {
+			if (document.documentElement.scrollTop > finalPosition - (scrollFragment / 2)) clearInterval(animatedScroll);
+		} else {
+			if (document.documentElement.scrollTop < finalPosition - (scrollFragment / 2)) clearInterval(animatedScroll);
+		}
+
 	}, 1);
 };
 
+const animatedScrollEvent = (originElement, time) => {
+	// Si el elemento es un enlace y empieza por almoadilla
+	if (originElement.tagName === 'A' && originElement.hash !== '') {
+		let targetElement = document.getElementById(originElement.hash.slice(1));
+		originElement.addEventListener('click', e => {
+			e.preventDefault();
+			animatedScrollTo(targetElement,time);
+		});
+	}
+};
 
+// Para animar todos los links que haya
+const animatedScrollAllLinks = time => {
+	let links = document.links;
+	for (let link of links) {
+		animatedScrollEvent(link, time);
+	}
+};
 
-animatedScrollTo(document.getElementById('cap2'), 500);
+// Para un solo elemento en especifico
+// animatedScrollEvent(document.getElementById('link2'), 500);
 
-// let animatedScroll = setInterval(function(){
-	// document.body.scrollTop += 10;
-
-	// // y =+ 10;
-	// // window.scrollBy(0, y)
-// }, 10);
+// Para todos los links
+animatedScrollAllLinks(200);
--- Proyecto Final/public/js/animated-scroll.js
+++ Proyecto Final/public/js/animated-scroll.js
@@ -1,39 +1,155 @@
 'use strict';
 
-// let y = 0;
+// CODIGO DESCARGADO
 
-// document.body.scrollTop;
+// const getInitialScroll = () => document.documentElement.scrollTop;
+// const getFinalScroll = element => Math.floor(element.getBoundingClientRect().top + getInitialScroll());
+
+// const animatedScrollTo = (targetElement,time) => {
+//   let initialPosition = getInitialScroll(),
+//       finalPosition = getFinalScroll(targetElement),
+//       distanceToScroll = finalPosition - initialPosition,
+//       scrollFragment = Math.ceil(distanceToScroll / time);
+//   animateScroll(scrollFragment, finalPosition);
+// };
+
+// const animateScroll = (scrollFragment,finalPosition) => {
+//   let animatedScroll = setInterval(function(){
+//     document.documentElement.scrollTop += scrollFragment;
+//     if (scrollFragment > 0) {
+//       if (document.documentElement.scrollTop > finalPosition - (scrollFragment / 2)) clearInterval(animatedScroll)
+//     } else {
+//       if (document.documentElement.scrollTop < finalPosition - (scrollFragment / 2)) clearInterval(animatedScroll)
+//     }
+
+//   },1);
+// };
+
+// const animatedScrollEvent = (originElement,time) => {
+//   if (originElement.tagName === 'A' && originElement.hash !== '') {
+//     let targetElement = document.getElementById(originElement.hash.slice(1));
+//     originElement.addEventListener('click', e => {
+//       e.preventDefault();
+//       animatedScrollTo(targetElement,time)
+//     })
+//   }
+// };
+
+// const animatedScrollAllLinks = time => {
+//   let links = document.links;
+//   for (let link of links) {
+//     animatedScrollEvent(link,time)
+//   }
+// };
+
+// animatedScrollAllLinks(200);
+
+// animatedScrollEvent(document.getElementById('link2'),500);
+
+
+// TERMINA CODIGO DESCARGADO
+
+
+/**********************************************/
+
+// console.log(document.documentElement.scrollTop);
+// console.log(window.scrollY);
+
+// let y = 0;
+// let animatedScroll = setInterval(function(){
+// document.documentElement.scrollTop += 10;
+
+// Con window seria diferente es mas sencillo con document.documentElementt
+// y += 10;
+// window.scrollBy(0, y);
+
+// }, 10);
+
 
 var getInitialScroll = function getInitialScroll() {
-	return window.scrollY;
+	return document.documentElement.scrollTop;
 };
-// console.log(getScroll());
+// getBoundingClientRect detecta las coordenadas respecto al viewport
 var getFinalScroll = function getFinalScroll(element) {
 	return Math.floor(element.getBoundingClientRect().top + getInitialScroll());
 };
 
 // console.log(getFinalScroll(document.getElementById('cap2')));
 
-var animatedScrollTo = function animatedScrollTo(element, time) {
-	var initialPosition = getInitialScroll(),
-	    finalPosition = getFinalScroll(element),
-	    distanceToScroll = finalPosition - initialPosition,
-	    scrollFragment = distanceToScroll / time;
+// Se necesita una unidad de tiempo para hacer la animacion
 
-	animatedScroll(scrollFragment);
+var animatedScrollTo = function animatedScrollTo(targetElement, time) {
+	var initialPosition = getInitialScroll(),
+	    finalPosition = getFinalScroll(targetElement),
+	    distanceToScroll = finalPosition - initialPosition,
+
+
+	// Si un auto recorre 10km (una distancia) en 1min (un tiempo) puedo obtener cuanto recorre por segundo
+	// supongamos que obtenemos 10m por segundo con Distancia / tiempo
+	scrollFragment = distanceToScroll / time;
+
+	animateScroll(scrollFragment, finalPosition);
+	// console.log(scrollFragment);
 };
 
-var animateScroll = function animateScroll(scrollFragment) {
+var animateScroll = function animateScroll(scrollFragment, finalPosition) {
+	// Recorrera el fragmento cada 1ms
 	var animatedScroll = setInterval(function () {
-		window.scrollY += scrollFragment;
+		document.documentElement.scrollTop += scrollFragment;
+
+		// Nos va dando la posicion del elemento
+		// console.log(document.documentElement.scrollTop);
+
+		if (scrollFragment > 0) {
+			if (document.documentElement.scrollTop > finalPosition - scrollFragment / 2) clearInterval(animatedScroll);
+		} else {
+			if (document.documentElement.scrollTop < finalPosition - scrollFragment / 2) clearInterval(animatedScroll);
+		}
 	}, 1);
 };
 
-animatedScrollTo(document.getElementById('cap2'), 500);
+var animatedScrollEvent = function animatedScrollEvent(originElement, time) {
+	// Si el elemento es un enlace y empieza por almoadilla
+	if (originElement.tagName === 'A' && originElement.hash !== '') {
+		var targetElement = document.getElementById(originElement.hash.slice(1));
+		originElement.addEventListener('click', function (e) {
+			e.preventDefault();
+			animatedScrollTo(targetElement, time);
+		});
+	}
+};
 
-// let animatedScroll = setInterval(function(){
-// document.body.scrollTop += 10;
+// Para animar todos los links que haya
+var animatedScrollAllLinks = function animatedScrollAllLinks(time) {
+	var links = document.links;
+	var _iteratorNormalCompletion = true;
+	var _didIteratorError = false;
+	var _iteratorError = undefined;
 
-// // y =+ 10;
-// // window.scrollBy(0, y)
-// }, 10);
+	try {
+		for (var _iterator = links[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+			var link = _step.value;
+
+			animatedScrollEvent(link, time);
+		}
+	} catch (err) {
+		_didIteratorError = true;
+		_iteratorError = err;
+	} finally {
+		try {
+			if (!_iteratorNormalCompletion && _iterator.return) {
+				_iterator.return();
+			}
+		} finally {
+			if (_didIteratorError) {
+				throw _iteratorError;
+			}
+		}
+	}
+};
+
+// Para un solo elemento en especifico
+// animatedScrollEvent(document.getElementById('link2'), 500);
+
+// Para todos los links
+animatedScrollAllLinks(200);
